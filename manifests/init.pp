271becea802a347f1ad30b3f76b05e4045883201
Reid Vandewiele <reid@puppetlabs.com>
2017-01-20 14:34:14 -0800

    Make output of puppetdb metrics cleaner
    Changes:
    
      * Change "puppet_server" to "puppetserver" when referring to the
        product and naming files. This is consistent with how puppetserver
        is referred to in product and documentation.
      * Refactor puppetserver metrics script to store all metrics for a
        timestamp in a single JSON file, even when multiple servers are
        queried
      * Refactor puppetdb metrics script to store all metrics for a
        timestamp in a single JSON file, even when multiple servers are
        queried
      * Standardize file timestamp format to use ISO 8601
      * Standardize filename to always be ${timestamp}.json
      * Move data out of puppetdb template and into manifest. Adheres to
        best practices for templates, which encourage keeping data and code
        out of templates as much as possible.
    
    These changes are targeted at simplifying data upload into Graphite /
    Grafana.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class pe_metric_curl_cron_jobs (
   String        $output_dir                   = '/opt/puppetlabs/pe_metric_curl_cron_jobs',
-  String        $puppet_server_metrics_ensure = 'present',
-  Array[String] $puppet_server_hosts          = [ '127.0.0.1' ],
+  String        $puppetserver_metrics_ensure  = 'present',
+  Array[String] $puppetserver_hosts           = [ '127.0.0.1' ],
   String        $puppetdb_metrics_ensure      = 'present',
   Array[String] $puppetdb_hosts               = [ '127.0.0.1' ],
 ) {
 
-  $scripts_dir        = "${output_dir}/scripts"
+  $scripts_dir = "${output_dir}/scripts"
 
   file { [ $output_dir, $scripts_dir ] :
     ensure => directory,
@@ -17,14 +17,62 @@ class pe_metric_curl_cron_jobs (
     scripts_dir => $scripts_dir,
   }
 
-  pe_metric_curl_cron_jobs::pe_metric { 'puppet_server' :
-    metric_ensure => $puppet_server_metrics_ensure,
-    hosts         => $puppet_server_hosts,
+  pe_metric_curl_cron_jobs::pe_metric { 'puppetserver' :
+    metric_ensure => $puppetserver_metrics_ensure,
+    hosts         => $puppetserver_hosts,
+  }
+
+  $pdb_metrics_base = [
+    { 'name' => 'commands_queue',
+      'url'  => 'org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=puppetlabs.puppetdb.commands' },
+    { 'name' => 'catalog_hash_miss',
+      'url'  => 'puppetlabs.puppetdb.storage%3Aname%3Dcatalog-hash-miss-time' },
+    { 'name' => 'catalog_hash_match',
+      'url'  => 'puppetlabs.puppetdb.storage%3Aname%3Dcatalog-hash-match-time' },
+    { 'name' => 'replace_catalog_time',
+      'url'  => 'puppetlabs.puppetdb.storage%3Aname%3Dreplace-catalog-time' },
+    { 'name' => 'replace_facts_time',
+      'url'  => 'puppetlabs.puppetdb.storage%3Aname%3Dreplace-facts-time' },
+    { 'name' => 'store_report_time',
+      'url'  => 'puppetlabs.puppetdb.storage%3Aname%3Dstore-report-time' },
+    { 'name' => 'global_retried',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dglobal.retried' },
+    { 'name' => 'global_retry_counts',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dglobal.retry-counts' },
+    { 'name' => 'global_retry_persistence_time',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dglobal.retry-persistence-time' },
+  ]
+
+  $pdb_metrics_20164 = [
+    { 'name' => 'replace_catalog_retried',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+catalog.9.retried' },
+    { 'name' => 'replace_catalog_retry_counts',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+catalog.9.retry-counts' },
+    { 'name' => 'replace_catalog_retry_persistence_time',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+catalog.9.retry-persistence-time' },
+    { 'name' => 'replace_facts_retried',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+facts.5.retried' },
+    { 'name' => 'replace_facts_retry_counts',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+facts.5.retry-counts' },
+    { 'name' => 'replace_facts_retry_persistence_time',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dreplace+facts.5.retry-persistence-time' },
+    { 'name' => 'store_report_retried',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dstore+report.8.retried' },
+    { 'name' => 'store_reports_retry_counts',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dstore+report.8.retry-counts' },
+    { 'name' => 'store_report_retry_persistence_time',
+      'url'  => 'puppetlabs.puppetdb.mq%3Aname%3Dstore+report.8.retry-persistence-time' },
+  ]
+
+  $pdb_metrics_array = $::pe_server_version ? {
+    /^2016.4./ => $pdb_metrics_base + $pdb_metrics_20164,
+    default    => $pdb_metrics_base
   }
 
   pe_metric_curl_cron_jobs::pe_metric { 'puppetdb' :
     metric_ensure => $puppetdb_metrics_ensure,
     hosts         => $puppetdb_hosts,
+    data          => $pdb_metrics_array,
   }
 
 }
