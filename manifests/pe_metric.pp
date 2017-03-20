fa12e32075cd27a7358c6e33e1e5cc8334f63747
Nick Walker <nick.walker@puppetlabs.com>
2017-03-20 09:10:51 -0700

    Combine puppetdb and puppetserver metrics script into one
    Prior to this commit, the puppetserver and puppetdb metrics
    gathering scripts were seperate.  However, after adding the status
    endpoint to puppetdb metrics tracking it became clear that these
    two scripts should be the same.
    
    After this commit, there is one template for making the metrics
    gathering script.

--- manifests/pe_metric.pp
+++ manifests/pe_metric.pp
@@ -2,6 +2,7 @@ define pe_metric_curl_cron_jobs::pe_metric (
   Enum['absent', 'present'] $metric_ensure  = 'present',
   String                    $output_dir,
   String                    $scripts_dir,
+  Integer                   $metrics_port,
   String                    $metrics_type   = $title,
   Array[String]             $hosts          = [ '127.0.0.1' ],
   String                    $cron_minute    = '*/5',
@@ -22,9 +23,11 @@ define pe_metric_curl_cron_jobs::pe_metric (
   file { $script_file_name :
     ensure  => $metric_ensure,
     mode    => '0744',
-    content => epp("pe_metric_curl_cron_jobs/${metrics_type}_metrics.epp", {
+    content => epp("pe_metric_curl_cron_jobs/metrics.epp", {
       'output_dir'    => $metrics_output_dir,
       'hosts'         => $hosts,
+      'metrics_type'  => $metrics_type,
+      'metrics_port'  => $metrics_port,
     }),
   }
 
