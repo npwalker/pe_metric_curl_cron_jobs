c2fc9c43aa9d31c9ab5ec163c6b2ddb164bba6f8
Nick Walker <nick.walker@puppetlabs.com>
2017-04-14 10:01:12 -0700

    Move metrics array out of the activemq script template
    This commit moves the metrics array form the script template into
    the puppet class so that it can be passed through the additional
    metrics parameter that is already in use with the PuppetDB
    class.

--- manifests/activemq.pp
+++ manifests/activemq.pp
@@ -12,10 +12,49 @@ class pe_metric_curl_cron_jobs::activemq (
     retention_days => $retention_days,
   }
 
+  $additional_metrics = [
+    {
+      'type'      => 'read',
+      'mbean'     => 'java.lang:type=Memory',
+      'attribute' => 'HeapMemoryUsage,NonHeapMemoryUsage'
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'java.lang:name=*,type=GarbageCollector',
+      'attribute' => 'CollectionCount'
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'java.lang:type=Runtime',
+      'attribute' => 'Uptime'
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'java.lang:type=OperatingSystem',
+      'attribute' => 'OpenFileDescriptorCount,MaxFileDescriptorCount'
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'org.apache.activemq:brokerName=*,type=Broker',
+      'attribute' => 'MemoryLimit,MemoryPercentUsage,CurrentConnectionsCount'
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'org.apache.activemq:type=Broker,brokerName=*,destinationType=Queue,destinationName=mcollective.*',
+      'attribute' => 'AverageBlockedTime,AverageEnqueueTime,AverageMessageSize,ConsumerCount,DequeueCount,DispatchCount,EnqueueCount,ExpiredCount,ForwardCount,InFlightCount,ProducerCount,QueueSize',
+    },
+    {
+      'type'      => 'read',
+      'mbean'     => 'org.apache.activemq:type=Broker,brokerName=*,destinationType=Topic,destinationName=mcollective.*.agent',
+      'attribute' => 'AverageBlockedTime,AverageEnqueueTime,AverageMessageSize,ConsumerCount,DequeueCount,DispatchCount,EnqueueCount,ExpiredCount,ForwardCount,InFlightCount,ProducerCount,QueueSize',
+    },
+  ]
+
   pe_metric_curl_cron_jobs::pe_metric { 'activemq' :
     metric_ensure          => $metrics_ensure,
     hosts                  => $hosts,
     metrics_port           => $port,
     metric_script_template => 'activemq_metrics.epp',
+    additional_metrics     => $additional_metrics,
   }
 }
